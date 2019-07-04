cb4955cc8c8e1da972e8492b25ef2a68623ad206
Francisco <javiermartincuenca@gmail.com>
2019-07-04 17:23:36 +0200

    Dices change when button is pressed

--- Dices/ViewController.swift
+++ Dices/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageViewDiceLeft: UIImageView!
+    @IBOutlet weak var ImageViewDiceRight: UIImageView!
+    
+    var randomDiceIndexLeft: Int = 0
+    var randomDiceIndexRight: Int = 0
+    let diceImages:[String]
+    let nFaces: UInt32
+    
+    required init?(coder aDecoder: NSCoder) {
+        diceImages = (1...6).map{"dice\($0)"}
+        nFaces = UInt32(diceImages.count)
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.generateRandomNumber()
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        self.generateRandomNumber()
+    }
+    
+    private func generateRandomNumber() {
+        randomDiceIndexLeft = Int(arc4random_uniform(self.nFaces))
+        randomDiceIndexRight = Int(arc4random_uniform(self.nFaces))
+        ImageViewDiceLeft.image = UIImage(named: diceImages[randomDiceIndexLeft])
+        ImageViewDiceRight.image = UIImage(named: diceImages[randomDiceIndexRight])
+    }
 }
 
